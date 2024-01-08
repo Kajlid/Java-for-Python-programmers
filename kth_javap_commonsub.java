cce3a57248715b11367778c56176585402bfaa6b
Kajlid <104261817+Kajlid@users.noreply.github.com>
2024-01-08 12:38:03 +0100

    Fixat fall med mellanslag och specialtecken orderedString

--- kth_javap_commonsub.java
+++ kth_javap_commonsub.java
@@ -1,5 +1,13 @@
 public class kth_javap_commonsub {
     public static void main (String[] args){
 
+        String S = "ABCDABCDABADAB";
+        Integer n = 3;      // Integer makes n an object, int would make it a primitive
+        // hard to combine primitives and objects
+
+        String order = "0123456789ABCDEGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz";
+
+
+
     }
 }
--- orderedString.java
+++ orderedString.java
@@ -1,11 +1,22 @@
 public class orderedString {
     public static void main(String[] args) {
-        String S = "DCBABCBA";
+        String S = "123abcABCDabcd1234";
         System.out.println(findLongestSortedSubstring(S)); 
     }
 
     public static int findLongestSortedSubstring(String s) {
+
+        if (s.isEmpty()) {
+            return 0;  // Om strängen är tom returnerar vi 0
+        }
+
         int L = 1;
+        String[] parts = s.split(" ");
+        
+        // Ta bort mellanslaget och sätt ihop strängen igen
+        s = String.join("", parts);
+        s = s.replaceAll("[^0-9A-Za-z]", ""); // ta bort alla specialtecken
+
         int currentSubstring = 1;
         for (int i = 1; i < s.length(); i++) {
             if (compareChars(s.charAt(i), s.charAt(i - 1)) >= 0) {
@@ -20,6 +31,13 @@ public class orderedString {
 
     private static int compareChars(char a, char b) {
         String order = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        return order.indexOf(a) - order.indexOf(b);
+        int indexA = order.indexOf(a);
+        int indexB = order.indexOf(b);
+
+        if (indexA == -1 || indexB == -1) {  // om tecken som inte finns med i ordningen skrivs med
+            return 0; // Ignorera specialtecken genom att returnera 0 istället
+        }
+
+        return indexA - indexB;
     }
 }
