99de3360e64faaf4816e79ee0616b0569e34d65a
Kajlid <104261817+Kajlid@users.noreply.github.com>
2024-01-16 10:06:44 +0100

    Create kth_javap_basen.java

---
+++ kth_javap_basen.java
@@ -0,0 +1,16 @@
+public class kth_javap_basen {
+
+    public static void main (String[] args) {
+
+        /*  
+        I indata anges först talen N resp. M på första raden, sedan följer talen A och B på varsin egen rad.
+        Du kan anta att N och M är heltal, och minst 2 och högst 36, samt att både A och B är positiva heltal som är inte är större än Integer.MAX_VALUE.
+
+        Utdata: Ett heltal C angett i bas M.
+
+        */
+
+
+    }
+    
+}
