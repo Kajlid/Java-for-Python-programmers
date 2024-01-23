c97e9dc53d9717cb2143dfc6c608d3feb0dc056e
Kajlid <104261817+Kajlid@users.noreply.github.com>
2024-01-23 16:15:18 +0100

    Update kth_javap_pathfinder.java

--- kth_javap_pathfinder.java
+++ kth_javap_pathfinder.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class kth_javap_pathfinder {
 
+    /*
+     Programmet ska skriva ut alla bokstäver (i bokstavsordning) 
+     som förbinder kartans översta och understa rad med varandra via en eller flera stigar
+     Två rutor i kartan anses tillhöra samma stig om de ligger antingen direkt ovanför/under, eller bredvid varandra. 
+     Inga diagonaler räknas alltså.
+     Om det inte existerar några stigar alls, ska programmet skriva ut "0" (en nolla).
+
+     I indata anges först två heltal, som anger antalet rader (M), resp kolumner (N), i den ordningen. På raden därefter följer själva kartan, som M rader med N tecken i varje rad.
+
+     Du kan anta att N och M är heltal, och minst 1 och högst 10 000, samt att kartan består av versaler.
+
+
+     */
+
     public static void main (String[] args) {
+        Scanner scanObj = new Scanner(System.in);
+
+
 
     }
     
