e789b3479ccd0140a56baec7b6ebd7fb5a38a9ba
Kajlid <104261817+Kajlid@users.noreply.github.com>
2024-04-24 13:45:31 +0200

    Förberedd labb 3 för presentation

--- kth_javap_basen.java
+++ kth_javap_basen.java
@@ -3,41 +3,33 @@ import java.math.BigInteger;
 
 public class kth_javap_basen {
     /*  
-        I indata anges först talen N resp. M på första raden, sedan följer talen A och B på varsin egen rad.
-        Du kan anta att N och M är heltal, och minst 2 och högst 36, samt att både A och B är positiva heltal som är inte är större än Integer.MAX_VALUE.
+        Labb 3:
+            I indata anges först talen N resp. M på första raden, sedan följer talen A och B på varsin egen rad.
+            Du kan anta att N och M är positiva heltal, och minst 2 och högst 36, inte är större än Integer.MAX_VALUE.
 
-        Utdata: Ett heltal C angett i bas M.
+            Utdata: Ett heltal C angett i bas M (produkten av A och B).
 
         */
 
-    
-    /*
-        Formatting numeric print output:
-
-        PrintStream class has two formatting methods that can be used to replace print and println:
-
-        format and printf. Equivalent to each other. Can be used instead of print and println.
-
-`       public PrintStream format(String format, Object... args)`
-     */
 
     public static void main (String[] args) {
 
         Scanner scanObj = new Scanner(System.in);
 
-        System.out.println("N och M");
+        // Skriv först bas N och M med mellan slag emellan och radbryt efter
         int N = scanObj.nextInt();
         int M = scanObj.nextInt();
         scanObj.nextLine();
-        System.out.println("A");
-        BigInteger A = new BigInteger(scanObj.next().trim(), N);
-        System.out.println("B");
+
+        // Skriv nu heltal A, följt av radbryt
+        // A och B definieras för att tillhöra bas N
+        BigInteger A = new BigInteger(scanObj.next().trim(), N);    
+        // Skriv sist heltal B
         BigInteger B = new BigInteger(scanObj.next().trim(), N);
         scanObj.close();
 
         String result = javap_basen(N, M, A, B);
         System.out.println(result);
-
 
     }
 
