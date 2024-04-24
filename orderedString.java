e3713d4e84af0eede7c7b36bc3a018471a9c8ab6
Kajlid <104261817+Kajlid@users.noreply.github.com>
2024-04-24 09:37:43 +0200

    Labb 1, skriv tydliga kommentarer

--- orderedString.java
+++ orderedString.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
 
+/*
+ Labb 1:
+    Hitta längsta sorterade delsträngen i en längre sträng.
+    Indata: icke radbruten sträng S. 
+    Utdata: Heltal L (längd på längsta sorterade delsträng)
+ */
+
 public class orderedString {
     public static void main(String[] args) {
         Scanner scanObj = new Scanner(System.in);     // öppnar ett object av klass scanner
+        // Hjälpsträng för att komma ihåg input-typ, får inte skickas in i Kattis
         System.out.println("Skriv en sträng:");
         String S = scanObj.nextLine().trim();       // vad användaren skriver in
         scanObj.close();
-        int L = alphaSub(S);
+        int L = alphaSub(S);    // utdata
         System.out.println(L); 
-
-        
-
    
     }
 
     public static int alphaSub(String s) {
 
+
         int length = 1;
 
         if (s.isEmpty()) {
-            return 0;  // Om strängen är tom returnerar vi 0
+            return 0;      // Om strängen är tom, returnera 0 direkt
         }
 
         int curr = 1;
