fb810acf8a5bc90b8e287923dd50a6467f76d33b
Kajlid <104261817+Kajlid@users.noreply.github.com>
2024-05-07 14:37:42 +0200

    Rensa kommentarer pathfinder

--- kth_javap_pathfinder.java
+++ kth_javap_pathfinder.java
@@ -1,22 +1,5 @@
 import java.util.*;
 
-/* 
- Labb 5:
-Kartan består av en matris med M rader och N kolumner, innehållandes bokstäverna A-Z. 
-Programmet ska skriva ut alla bokstäver (i bokstavsordning) som förbinder kartans översta och understa rad med varandra via en eller flera stigar. 
-Två rutor i kartan anses tillhöra samma stig om de ligger antingen direkt ovanför/under, eller bredvid varandra. Inga diagonaler räknas alltså.
-
-Om det inte existerar några stigar alls, ska programmet skriva ut "0" (en nolla).
-
-Indata: 
-    I indata anges först två heltal, som anger antalet rader (M), resp kolumner (N), i den ordningen. 
-    På raden därefter följer själva kartan, som M rader med N tecken i varje rad.
-
-Utdata:
-    En sträng av bokstäver i alfabetisk ordning, eller "0".
-
- */
-
 public class Kth_javap_pathfinder {
     // Fält som tillhör hela klassen, static' (tillhör klassen själv), går snabbare - slipper definera flera gånger
     static char[][] map;
